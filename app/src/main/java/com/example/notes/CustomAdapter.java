71b542f030e9f2af531ace1533bf064628b9280d
Manu <mansivaghela804@gmail.com>
2019-02-22 20:32:53 +0530

    Initial Commit

--- app/src/main/java/com/example/notes/CustomAdapter.java
+++ app/src/main/java/com/example/notes/CustomAdapter.java
@@ -54,6 +54,7 @@ public class CustomAdapter extends BaseAdapter {
                 });
         String note = users.get(position);
         tv.setText(note);
+        btn.setText(note);
         return view;
     }
 }
